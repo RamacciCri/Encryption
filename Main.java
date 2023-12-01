0399a0c41cf29101f60c1775a339e7089cbaa728
RamacciCri <cristianramacci56@gmail.com>
2023-12-01 13:40:28 +0000

    caricamento classe avvio

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Cifriamo il nostro testo e trasformiamolo in un testo segreto!");
+    Matrice m = new Matrice("TPSIT");
+    Vigenere v = new Vigenere(0, 12, 0, 12, m);
+    Thread t = new Thread(v);
+    t.start();
+  }
+}
