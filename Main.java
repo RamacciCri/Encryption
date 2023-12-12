e1b812851aadee9168dde877ef2a0117ef844b52
RamacciCri <152168033+RamacciCri@users.noreply.github.com>
2023-12-12 10:38:18 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
 class Main {
   public static void main(String[] args) {
+    //inserire parola chiave
+    String parolaChiave;
+    System.out.println("Inserisci la parola chiave:");
+    try {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      parolaChiave = br.readLine();
+    }
+    catch (IOException){
+      System.err.println("Errore lettura dati da tastiera");
+    }
     System.out.println("Cifriamo il nostro testo e trasformiamolo in un testo segreto!");
     Matrice m = new Matrice("TPSIT");
     Vigenere v = new Vigenere(0, 12, 0, 12, m);
